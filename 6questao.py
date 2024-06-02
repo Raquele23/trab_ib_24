09a5d2ba4c49d5621e97eed225a0c6fe6c2e8d43
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2024-06-02 15:06:33 -0300

    Update 6questao.py

--- 6questao.py
+++ 6questao.py
@@ -1,7 +1,9 @@
-6questao -teste-
+6questao
 cidadão = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
-if idade>=16:
+if idade >= 16 and idade <18:
 	print("Você já pode emitir seu título!")
+elif idade >= 18:
+	print("Você já deveria ter tirado seu título!")
 else:
 	print("Você ainda não pode emitir seu título!")

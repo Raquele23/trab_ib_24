d61f7cff611a69016653aaf77e5605636837464d
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2024-06-03 19:49:10 -0300

    Update 08questao.py

--- 08questao.py
+++ 08questao.py
@@ -1,6 +1,6 @@
-8questao
-n1 = int(input("Digite um número de 1 a 10: "))
-n2 = int(input("Digite um número de 1 a 10: "))
+n1 = int(input("Escolha um número de 1 a 100: "))
+n2 = int(input("Escolha um número de 1 a 100: "))
+print("        ")
 print(f"A soma desses números é: {n1+n2}")
 print(f"A subtração desses números é: {n1-n2}")
 print(f"O produto desses números é: {n1*n2}")

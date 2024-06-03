21b2b36a631473334779f44e1a4da5cc613049bf
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2024-06-02 21:18:26 -0300

    Update 07questao.py

--- 07questao.py
+++ 07questao.py
@@ -1,12 +1,17 @@
 7questao
 np = int(input("Escolha um número de 0 a 10: "))
 nb = int(input("Escolha um número de 0 a 10: "))
+print(".               ")
+print(".               ")
+print(".               ")
 print("BOLETIM ESCOLAR")
-aluno = input("Digite o nome do aluno: ")
-disciplina  = input("Digite o nome da disciplina: ")
+aluno = input("Digite o nome e sobrenome do aluno: ")
+disciplina = input("Digite o nome da disciplina: ")
+print("                ")
 print(f"A nota parcial do aluno foi {np + .0} e a nota bimestral {nb + .0}")
+print("                ")
 media = (np + nb)/2
 if media >= 6:
-  print(f"{aluno} ficou com a média de {media}, portanto está APROVADO na disciplina de {disciplina}!")
+  print(f"{aluno} ficou com a média: {media}. Então está APROVADO na disciplina de {disciplina}!")
 else:
-  print(f"{aluno} ficou com a média de {media}, portanto está REPROVADO na disciplina de {disciplina}!")
+  print(f"{aluno} ficou com a média: {media}. Então está REPROVADO na disciplina de {disciplina}!")

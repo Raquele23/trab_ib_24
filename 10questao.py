1c6b36cf7177daaaf00410bd1e4b93826fbb7aba
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2024-06-03 20:02:06 -0300

    Update 10questao.py

--- 10questao.py
+++ 10questao.py
@@ -1,4 +1,3 @@
-10questao
-num = int(input("Escolha um número de 1 a 5: "))
+num = int(input("Escolha um número: "))
 for r in range(1,11):
-  print(num, "x", r, "=", (num*r))
+  print(num, "x", r, "=", (num*r)) 

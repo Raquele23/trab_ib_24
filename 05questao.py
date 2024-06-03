45459c5c65b961bb9eef2f531cd64bd5c2ceb57c
Raquele23 <raquel.evangelista2@aluno.ce.gov.br>
2024-06-03 19:33:55 -0300

    Update 05questao.py

--- 05questao.py
+++ 05questao.py
@@ -1,8 +1,7 @@
-5questao
 hora = int(input("Diga um número entre 0 e 23: "))
 if hora >= 5 and hora < 12:
-	print("Está de manhã")
+	print("Está de manhã!")
 elif hora >= 12 and hora <= 17:
-	print("Já está de tarde")	
+	print("Já está de tarde!")	
 else:
-	print("Está de noite")
+	print("Está de noite!")
